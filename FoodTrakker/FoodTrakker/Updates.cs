22898747532994bc23711ffb8d4a20b4040001ef
Natalia Cygan <ncygan2@gmail.com>
2022-05-15 17:48:26 +0200

    use linq in updates

--- FoodTrakker/FoodTrakker/Updates.cs
+++ FoodTrakker/FoodTrakker/Updates.cs
@@ -12,17 +12,21 @@ namespace FoodTrakker.BusinessLogic
 
         public void Update(FoodTruck foodTruckToUpdate)
         {
-            foreach (var foodTruck in foodTrucks)
+            var foodTruck = foodTrucks.FirstOrDefault(f => f.ID == foodTruckToUpdate.ID);
+            if (foodTruck == null)
             {
-                if(foodTruck.ID == foodTruckToUpdate.ID)
-                {
-                    foodTruck.Owner = foodTruckToUpdate.Owner;
-                    foodTruck.Type = foodTruckToUpdate.Type;
-                    foodTruck.Description = foodTruckToUpdate.Description;  
-                    foodTruck.Location = foodTruckToUpdate.Location;    
-                    foodTruck.Name = foodTruckToUpdate.Name;
-                }
+                Console.WriteLine("Your FoodTruck doesn't exist.Please choose Add. ");
             }
+            else
+            {
+                foodTruck.Owner = foodTruckToUpdate.Owner;
+                foodTruck.Type = foodTruckToUpdate.Type;
+                foodTruck.Description = foodTruckToUpdate.Description;
+                foodTruck.Location = foodTruckToUpdate.Location;
+                foodTruck.Name = foodTruckToUpdate.Name;
+            }
+
+
         }
     }
 }
