f31d2159c5bfe0ad2324730d71fe325fe47e92b8
Natalia Cygan <ncygan2@gmail.com>
2022-05-20 15:30:14 +0200

    naprawiony program

---
+++ FoodTrakker/FoodTrakker.GUI/Program.cs
@@ -0,0 +1,134 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading;
+using FoodTrakker.GUI;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static List<Option> options;
+        static void Main(string[] args)
+        {
+            UpdateFoodTruck update = new UpdateFoodTruck();
+            update.FoodTruckUpdate();
+            UpdateEvent updateEvent = new UpdateEvent();
+            updateEvent.EventUpdate();
+            UpdateReview updateReview = new UpdateReview();
+            updateReview.ReviewUpdate();
+            Console.WriteLine("Welcome in FoodTrakker App, press any key to enter the main menu.");
+            Console.WriteLine("Use arrows (UP and Down) to navigate on main menu.");
+            Console.ReadKey();
+
+            options = new List<Option>
+            {
+                new Option("Find FoodTruck", () => FindTruck("")),
+                new Option("Log-In", () =>  WriteTemporaryMessage("You are trying to Log-In")),
+                new Option("Create Account", () =>  WriteTemporaryMessage("You are tryinig to crate account")),
+                new Option("Exit", () => Environment.Exit(0)),
+            };
+
+            // Set the index of the selected item to be the first
+            int index = 0;
+
+            // Write the menu out
+            WriteMenu(options, options[index]);
+
+            // Store key info in here
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                keyinfo = Console.ReadKey();
+
+                // Handle each key input (down arrow will write the menu again with a different selected item)
+                if (keyinfo.Key == ConsoleKey.DownArrow)
+                {
+                    if (index + 1 < options.Count)
+                    {
+                        index++;
+                        WriteMenu(options, options[index]);
+                    }
+                }
+                if (keyinfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (index - 1 >= 0)
+                    {
+                        index--;
+                        WriteMenu(options, options[index]);
+                    }
+                }
+                // Handle different action for the option
+                if (keyinfo.Key == ConsoleKey.Enter)
+                {
+                    options[index].Selected.Invoke();
+                    index = 0;
+                }
+            }
+            while (keyinfo.Key != ConsoleKey.X);
+
+            Console.ReadKey();
+
+        }
+        // Default action of all the options. 
+        static void WriteTemporaryMessage(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            Thread.Sleep(3000);
+            WriteMenu(options, options.First());
+        }
+
+        static void FindTruck(string locationOfTruck)
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter name of your City to display all avalibe FoodTrucks in area\n");
+            locationOfTruck = Console.ReadLine();
+            Console.WriteLine($"Thank You! In your location: {locationOfTruck} we found (X) numbers of FoodTrucks ");
+            Console.WriteLine("Press any key to back to main menu");
+            Console.ReadKey();
+            WriteMenu(options, options.First());
+        }
+
+        static void LogInOption()
+        {
+            Console.Clear();
+
+
+        }
+
+
+
+        static void WriteMenu(List<Option> options, Option selectedOption)
+        {
+            Console.Clear();
+
+            foreach (Option option in options)
+            {
+                if (option == selectedOption)
+                {
+                    Console.Write("> ");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(option.Name);
+            }
+        }
+    }
+
+    public class Option
+    {
+        public string Name { get; }
+        public Action Selected { get; }
+
+        public Option(string name, Action selected)
+        {
+            Name = name;
+            Selected = selected;
+        }
+    }
+
+}
