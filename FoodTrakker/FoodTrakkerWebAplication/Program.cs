49225b0da10155b32c0379d20534e3b5deb690fd
MateuszB <mateuszbronclik@gmail.com>
2022-07-03 20:35:58 +0200

    trying to create db

--- FoodTrakker/FoodTrakkerWebAplication/Program.cs
+++ FoodTrakker/FoodTrakkerWebAplication/Program.cs
@@ -1,12 +1,18 @@
 using FoodTrakker_WebBusinessLogic;
 using FoodTrakker_WebBusinessLogic.Model;
 using FoodTrakker_WebBusinessLogic.Repository;
+using System.Data.Entity;
+using FoodTrakkerWebAplication.Data;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<FoodTrakkerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FoodTrakkerDb")));
 
 
 builder.Services.AddSingleton<IRepository<User>, UserRepository>();
@@ -22,6 +28,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
