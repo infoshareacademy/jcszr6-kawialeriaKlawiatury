a0fcc2ed24b3fba4e4df287737e53ca9bee32c1c
Natalia Cygan <ncygan2@gmail.com>
2022-07-24 20:58:38 +0200

    add mapping

---
+++ FoodTrakker/FoodTrakker.Services/Profiles/ReviewProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FoodTrakker.Core.Model;
+using FoodTrakker.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTrakker.Services.Profiles
+{
+    public class ReviewProfile : Profile
+    {
+        public ReviewProfile()
+        {
+            CreateMap<Review, ReviewDto>().ReverseMap();
+        } 
+    }
+}
