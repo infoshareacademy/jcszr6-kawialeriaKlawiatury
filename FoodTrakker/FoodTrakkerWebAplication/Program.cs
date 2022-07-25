88e5847f0f0915f2ef920248f762db07ee9c7ed5
Natalia Cygan <ncygan2@gmail.com>
2022-07-25 10:16:18 +0200

    aktualizacja mapowania

--- FoodTrakker/FoodTrakkerWebAplication/Program.cs
+++ FoodTrakker/FoodTrakkerWebAplication/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<FoodTruckService>();
