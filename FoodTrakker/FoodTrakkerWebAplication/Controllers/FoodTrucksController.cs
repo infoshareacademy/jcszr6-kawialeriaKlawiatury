2adcffea70e0f914bbb1935768c1476e3699c713
mark-savik <99361212+mark-savik@users.noreply.github.com>
2022-08-09 21:18:59 +0200

    add automapper for FoodTrucks

--- FoodTrakker/FoodTrakkerWebAplication/Controllers/FoodTrucksController.cs
+++ FoodTrakker/FoodTrakkerWebAplication/Controllers/FoodTrucksController.cs
@@ -1,25 +1,28 @@
-﻿using FoodTrakker.Core.Model;
+﻿using AutoMapper;
+using FoodTrakker.Core.Model;
 using Microsoft.AspNetCore.Mvc;
 using FoodTrakker.Services;
 using FoodTrakker.Repository;
+using FoodTrakker.Services.DTOs;
 
 namespace FoodTrakkerWebAplication.Controllers
 {
     public class FoodTrucksController : Controller
     {
         private readonly FoodTruckService _foodTruckService;
+        private readonly IMapper _mapper;
 
-        public FoodTrucksController(FoodTruckService foodTruckService)
+        public FoodTrucksController(FoodTruckService foodTruckService, IMapper mapper)
         {
             _foodTruckService = foodTruckService;
+            _mapper = mapper;
         }
-        //pobrać mapera i zrobic mapowanie
+        
         public async Task<ActionResult> Index()
         {
-           var foodTrucks = await _foodTruckService.GetFullFoodTruckInfoAsync();
-            //mapowanie gdzieś tutaj
-            var foodTruck = foodTrucks.OrderBy(ft => ft.Name).ToList();
-            return View(foodTrucks);
+            var foodTrucks = await _foodTruckService.GetFullFoodTruckInfoAsync();
+            var foodTruckDto = _mapper.Map<ICollection<FoodTruck>, ICollection<FoodTruckDto>>(foodTrucks);
+            return View(foodTruckDto);
         }
 
         //GET: FoodTrucksController/Details/5
