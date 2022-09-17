705856e2d51548694781efb2bf370c76f001c982
Natalia Cygan <ncygan2@gmail.com>
2022-09-17 11:16:58 +0200

    start adventure with api

--- FoodTrakker/FoodTrakker.Api/Program.cs
+++ FoodTrakker/FoodTrakker.Api/Program.cs
@@ -1,12 +1,27 @@
+using FoodTrakker.BusinessLogic.Models;
+using FoodTrakker.Repository;
+using FoodTrakker.Repository.Contracts;
+using FoodTrakker.Repository.Data;
+using FoodTrakker.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<EventService>();
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<FoodTrakkerContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FoodTrakkerDb"));
+    // options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
