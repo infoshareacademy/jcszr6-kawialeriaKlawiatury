d5fad48c38766baa8b675c9b40aecf878fca4f5b
Pawel Baran <pawelbaranc@gmail.com>
2022-09-20 17:26:38 +0200

    ProgramTest

---
+++ FoodTrakker/FoodTrakker.Api.IntegrationTests/ProgramTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTrakker.Api.IntegrationTests
+{
+    public class ProgramTests
+    {
+        private WebApplicationFactory<Program> _factory;
+        private readonly List<Type> _controllerTypes;
+
+        public ProgramTests()
+        {
+            _controllerTypes = typeof(Program)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(ControllerBase)))
+                .ToList();
+
+            _factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    _controllerTypes.ForEach(c => services.AddScoped(c));
+                });
+            });
+        }
+
+        [Fact]
+        public void ConfigureServices_ForControllers_RegistersAllDependencies()
+        {
+            var scopedFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopedFactory.CreateScope();
+
+
+            _controllerTypes.ForEach(t =>
+            {
+                var controller = scope.ServiceProvider.GetService(t);
+                controller.Should().NotBeNull();
+            });
+        }
+
+
+    }
+}
