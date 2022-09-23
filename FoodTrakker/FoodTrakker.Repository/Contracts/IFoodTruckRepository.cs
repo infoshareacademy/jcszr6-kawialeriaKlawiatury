04ef97fab4d5f7439f85bb3c81e8401a3d9202e9
mark-savik <99361212+mark-savik@users.noreply.github.com>
2022-09-23 23:02:36 +0200

    cleaning comments

--- FoodTrakker/FoodTrakker.Repository/Contracts/IFoodTruckRepository.cs
+++ FoodTrakker/FoodTrakker.Repository/Contracts/IFoodTruckRepository.cs
@@ -16,9 +16,8 @@ namespace FoodTrakker.Repository.Contracts
         public Task<List<FoodTruck>> FindFoodTruckAsync(string Name);
         public Task<List<FoodTruck>> FindByCityAsync(string City);
         public Task<List<FoodTruck>> FindByStreetAsync(string City);
-        
         public Task<List<FoodTruck>> FindByTypeAsync(string Type);
-        //public Task<List<FoodTruckEvent>> FindByEventAsync(string Event);
+        
         Task<(double, int)> AvgRatingAndReviewCount(int Id);
         Task<bool> HasFoodTruckReviewFromUser(int foodTruckId, string userId);
         Task<bool> IsAddedToFav(int id, string userId);
