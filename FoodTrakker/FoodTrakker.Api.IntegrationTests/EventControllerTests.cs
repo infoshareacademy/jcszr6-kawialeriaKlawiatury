04783c5ee704a69f2a636b161a2ad7863850f3bc
Pawel Baran <pawelbaranc@gmail.com>
2022-10-09 20:19:28 +0200

    GetByIdTest_Added

--- FoodTrakker/FoodTrakker.Api.IntegrationTests/EventControllerTests.cs
+++ FoodTrakker/FoodTrakker.Api.IntegrationTests/EventControllerTests.cs
@@ -60,17 +60,18 @@ namespace FoodTrakker.Api.IntegrationTests
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
 
-        [Fact]
-        public async Task GetEventById_ForValidRequest_ReturnOk()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async Task GetEventById_ForValidRequest_ReturnOk(int Id)
         {
             //arrange
-
             _eventRepository
-                .Setup(e => e.GetAsync(It.IsAny<int>()))
-                .Returns(Task.FromResult(FakeDbEvents.Events[0]));
+                .Setup(e => e.GetAsync(Id))
+                .Returns(Task.FromResult(FakeDbEvents.Events.Single(e => e.Id == Id)));
             //act
-            var response = await _client.GetAsync("/api/Event/1");
-
+            var response = await _client.GetAsync($"/api/Event/{Id}");
+            //var body = await response.Content.ReadFromJsonAsync<Event>();
             //assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
